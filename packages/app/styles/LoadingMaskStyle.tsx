33a1d68f03d6e5f3963edc05dad45f9ff73443ed
Barto <juanmartin.cerruti@gmail.com>
2022-08-03 21:38:15 -0300

    remove transition property

--- packages/app/styles/LoadingMaskStyle.tsx
+++ packages/app/styles/LoadingMaskStyle.tsx
@@ -1,7 +1,6 @@
 
 export const LoadingMaskStyle = {
   holder: {
-    transition: 'all 0.2s ease',
     position: 'absolute',
     top: 0, left: 0, right: 0, bottom: 0,
     backgroundColor: "black",
